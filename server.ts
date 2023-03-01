034eb867b766973a39d19432bae07f4ce92c0831
Diogo Eleutero <desdiogo10@yahoo.com>
2023-03-01 19:06:05 -0300

    fix response and

--- server.ts
+++ server.ts
@@ -9,17 +9,19 @@ type WebHookRequest = FastifyRequest<{
 }>
 
 server.post('/webhook', async (request: WebHookRequest, reply) => {
-  console.log(request.body)
   const { validationToken } = request.query
   const response = reply.code(200).header("Content-Type", "text/plain; charset=utf-8")
   if(!validationToken) {
-    return response
+    response
+  } else {
+    console.log('query', request.query)
+    response.send(validationToken)
   }
   
-  return response.send(validationToken)
+  response.send(validationToken)
 })
 
-server.listen({ port: process.env.PORT, host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: process.env.PORT }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
