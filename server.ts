64bc9fc1431a125743f3bba14c27957fc5b96b5b
Diogo Eleutero <desdiogo10@yahoo.com>
2023-03-01 18:56:45 -0300

    fix response and request

--- server.ts
+++ server.ts
@@ -1,12 +1,22 @@
-import fastify from 'fastify'
+import fastify, {FastifyRequest} from 'fastify'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 const server = fastify()
 
-server.post('/webhook', async (request, reply) => {
+type WebHookRequest = FastifyRequest<{
+  Querystring: { validationToken?: string }
+}>
+
+server.post('/webhook', async (request: WebHookRequest, reply) => {
   console.log(request.body)
-  reply.code(200).header("Content-Type", "text/plain; charset=utf-8")
+  const { validationToken } = request.query
+  const response = reply.code(200).header("Content-Type", "text/plain; charset=utf-8")
+  if(!validationToken) {
+    return response
+  }
+  
+  return response.send(validationToken)
 })
 
 server.listen({ port: process.env.PORT, host: '0.0.0.0' }, (err, address) => {
