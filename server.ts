43cbe1737ec17d960d1651aa8ee0a5cec925a786
Diogo Eleutero <desdiogo10@yahoo.com>
2023-03-01 20:33:14 -0300

    add prisma

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import fastify, {FastifyRequest} from 'fastify'
+import { PrismaClient } from '@prisma/client'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+const prisma = new PrismaClient()
 const server = fastify()
 
 type WebHookRequest = FastifyRequest<{
@@ -9,7 +11,14 @@ type WebHookRequest = FastifyRequest<{
 }>
 
 server.post('/webhook', async (request: WebHookRequest, reply) => {
-  console.log(request.body)
+  const body = request.body
+  if(body) {
+    await prisma.data.create({
+      data: {
+        data: body
+      }
+    })
+  }
   const { validationToken } = request.query
   const response = reply.code(200).header("Content-Type", "text/plain; charset=utf-8")
   if(!validationToken) {
